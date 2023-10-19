cdff2e7f20fb3c6d73b3d12de2ad2c1516973f5d
d038520 <148342722+d038520@users.noreply.github.com>
2023-10-19 19:52:28 +0800

    app.js

--- app.js
+++ app.js
@@ -7,3 +7,24 @@ app.post("/webhook", (req, res) => {
   console.dir(body, { depth: null });
     
 ...
+// Add support for GET requests to our webhook
+app.get("/messaging-webhook", (req, res) => {
+  
+// Parse the query params
+  let mode = req.query["hub.mode"];
+  let token = req.query["hub.verify_token"];
+  let challenge = req.query["hub.challenge"];
+
+  // Check if a token and mode is in the query string of the request
+  if (mode && token) {
+    // Check the mode and token sent is correct
+    if (mode === "subscribe" && token === config.verifyToken) {
+      // Respond with the challenge token from the request
+      console.log("WEBHOOK_VERIFIED");
+      res.status(200).send(challenge);
+    } else {
+      // Respond with '403 Forbidden' if verify tokens do not match
+      res.sendStatus(403);
+    }
+  }
+});
