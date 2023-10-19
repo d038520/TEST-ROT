0175cae2571934d2560f07712651fdea7be081c9
d038520 <148342722+d038520@users.noreply.github.com>
2023-10-19 19:53:42 +0800

    app.js

--- app.js
+++ app.js
@@ -28,3 +28,30 @@ app.get("/messaging-webhook", (req, res) => {
     }
   }
 });
+// Import dependencies and set up http server
+const express = require("express"),
+  bodyParser = require("body-parser"),
+  { urlencoded, json } = require("body-parser"),
+  app = express().use(bodyParser.json());
+    
+    ...
+
+
+// Verify that the callback came from Facebook.
+function verifyRequestSignature(req, res, buf) {
+  var signature = req.headers["x-hub-signature-256"];
+
+  if (!signature) {
+    console.warn(`Couldn't find "x-hub-signature-256" in headers.`);
+  } else {
+    var elements = signature.split("=");
+    var signatureHash = elements[1];
+    var expectedHash = crypto
+      .createHmac("sha256", config.appSecret)
+      .update(buf)
+      .digest("hex");
+    if (signatureHash != expectedHash) {
+      throw new Error("Couldn't validate the request signature.");
+    }
+  }
+}
